0e0103d9f29315e2d5702bf65ddb35846325967d
rizkiekowaluyo <mochrizkiekow@gmail.com>
2020-06-28 07:18:27 +0700

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,11 +41,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif                    
+                            @endif                     --}}
                         @endguest
                     </ul>
                 </div>
